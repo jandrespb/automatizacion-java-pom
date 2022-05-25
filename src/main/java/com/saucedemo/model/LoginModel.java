0f5efbe5c659e148217b8061ced59ede2a6b3659
JAndres PinillaB <andres_pb07@outlook.com>
2022-05-25 15:16:00 -0500

    rlogin: se configura la clase model y stepdefinition del login

--- src/main/java/com/saucedemo/model/LoginModel.java
+++ src/main/java/com/saucedemo/model/LoginModel.java
@@ -1,4 +1,21 @@
 package com.saucedemo.model;
 
 public class LoginModel {
+
+    String username, password;
+
+    // Constructor
+    public LoginModel(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- src/test/java/com/saucedemo/stepdefinition/LoginStepDefinition.java
+++ src/test/java/com/saucedemo/stepdefinition/LoginStepDefinition.java
@@ -1,4 +1,29 @@
 package com.saucedemo.stepdefinition;
 
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class LoginStepDefinition {
+
+    @Given("^User open browser on swag lab page$")
+    public void userOpenBrowserOnSwagLabPage() {
+
+    }
+
+    @And("^User validate credentials in login form$")
+    public void userValidateCredentialsInLoginForm() {
+
+    }
+
+    @When("^User access to home page successful$")
+    public void userAccessToHomePageSuccessful() {
+
+    }
+
+    @Then("^User log out and close browser$")
+    public void userLogOutAndCloseBrowser() {
+
+    }
 }
