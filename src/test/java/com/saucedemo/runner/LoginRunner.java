d08d05d6bb5d1068c89898da26888331252926cc
JAndres PinillaB <andres_pb07@outlook.com>
2022-05-25 15:07:51 -0500

    se modifica archivo build gradle

--- src/test/java/com/saucedemo/runner/LoginRunner.java
+++ src/test/java/com/saucedemo/runner/LoginRunner.java
@@ -1,12 +1,14 @@
 package com.saucedemo.runner;
 
-import io.cucumber.junit.CucumberOptions;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = "src/test/resources/com.saucedemo/feature/Login.feature",
 glue = "com.saucedemo.stepdefinition",
-snippets = CucumberOptions.SnippetType.CAMELCASE)
+snippets = SnippetType.CAMELCASE)
 public class LoginRunner {
 }
